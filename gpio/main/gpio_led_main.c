694d1fe0bcffb0faf7e024c91d1e0c4344673e5e
maker_knz <1461706290@qq.com>
2020-04-28 22:06:35 +0800

    完善gpio led程序

--- gpio/main/gpio_led_main.c
+++ gpio/main/gpio_led_main.c
@@ -1,11 +1,3 @@
-/* GPIO Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,45 +6,28 @@
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 
-/**
- * Brief:
- * This test code shows how to configure gpio and how to use gpio interrupt.
- *
- * GPIO status:
- * GPIO18: output
- * GPIO19: output
- * GPIO4:  input, pulled up, interrupt from rising edge and falling edge
- * GPIO5:  input, pulled up, interrupt from rising edge.
- *
- * Test:
- * Connect GPIO18 with GPIO4
- * Connect GPIO19 with GPIO5
- * Generate pulses on GPIO18/19, that triggers interrupt on GPIO4/5
- *
- */
-
-#define GPIO_OUTPUT_IO_0 18
-#define GPIO_OUTPUT_IO_1 19
-#define GPIO_OUTPUT_PIN_SEL ((1ULL << GPIO_OUTPUT_IO_0) | (1ULL << GPIO_OUTPUT_IO_1))
-#define GPIO_INPUT_IO_0 4
-#define GPIO_INPUT_IO_1 5
-#define GPIO_INPUT_PIN_SEL ((1ULL << GPIO_INPUT_IO_0) | (1ULL << GPIO_INPUT_IO_1))
 #define ESP_INTR_FLAG_DEFAULT 0
 
-#define GPIO_LED_SIGNAL 34
-gpio_num_t gpio_led_num = GPIO_NUM_32;
-gpio_mode_t gpio_led_model = GPIO_MODE_OUTPUT;
-gpio_num_t gpio_contact_switch_num = GPIO_NUM_33;
+gpio_num_t gpio_led_num = GPIO_NUM_32;            // 连接LED的GPIO
+gpio_num_t gpio_contact_switch_num = GPIO_NUM_33; // 连接触点开关GPIO
 
-static xQueueHandle gpio_evt_queue = NULL;
+static xQueueHandle gpio_evt_queue = NULL; // 事务队列
 
+int cnt = 0; // 计数，反转LED
+/**
+ * 中断处理
+ */
 static void IRAM_ATTR gpio_isr_handler(void *arg)
 {
+    cnt++;
+    // 反转gpio的电平，让LED灯出现显示的反转
+    gpio_set_level(gpio_led_num, cnt % 2);
     uint32_t gpio_num = (uint32_t)arg;
+    // 向gpio事件处理的队列中添加一条消息
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
-static void gpio_task_example(void *arg)
+static void gpio_task_led(void *arg)
 {
     uint32_t io_num;
     for (;;)
@@ -66,68 +41,21 @@ static void gpio_task_example(void *arg)
 
 void app_main(void)
 {
-    gpio_config_t io_conf;
-    //disable interrupt
-    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
+    // 设置控制LED的GPIO为输出模式
+    gpio_set_direction(gpio_led_num, GPIO_MODE_OUTPUT);
 
-    //interrupt of rising edge
-    io_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
-    //bit mask of the pins, use GPIO4/5 here
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    //set as input mode
-    io_conf.mode = GPIO_MODE_INPUT;
-    //enable pull-up mode
-    io_conf.pull_up_en = 1;
-    gpio_config(&io_conf);
-
-    //change gpio intrrupt type for one pin
-    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
-
-    // gpio_pullup_en(gpio_led_num);
-    gpio_set_direction(gpio_led_num, gpio_led_model);
-
-    // gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    gpio_int_type_t int_type = GPIO_INTR_POSEDGE;
+    // 设置作为中断的GPIO pin为输出模式
     gpio_set_direction(gpio_contact_switch_num, GPIO_MODE_INPUT);
-    gpio_set_intr_type(gpio_contact_switch_num, GPIO_INTR_ANYEDGE);
-    // gpio_pullup_en(gpio_contact_switch_num);
+    // 设置作为中断模式为沿上升沿触发
+    gpio_set_intr_type(gpio_contact_switch_num, GPIO_INTR_POSEDGE);
 
-    //create a queue to handle gpio event from isr
+    // 创建队列处理来自isr的gpio事件
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    //start gpio task
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+    // 启动gpio的任务
+    xTaskCreate(gpio_task_led, "gpio_task_led", 2048, NULL, 10, NULL);
 
-    //install gpio isr service
+    // 初始化全局GPIO的中断服务程序，并设置中断的电平等级
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *)GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void *)GPIO_INPUT_IO_1);
-
-    //remove isr handler for gpio number.
-    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin again
-    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *)GPIO_INPUT_IO_0);
-
+    // 将指定的GPIO挂载到中断服务上，并设定中断触发的回调函数和传入参数
     gpio_isr_handler_add(gpio_contact_switch_num, gpio_isr_handler, (void *)gpio_contact_switch_num);
-
-    int cnt = 0;
-    while (1)
-    {
-        printf("cnt: %d\n", cnt++);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        gpio_set_level(gpio_led_num, cnt % 2);
-        gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
-        gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
-    }
 }
